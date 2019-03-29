ef7efe26da6f6e93be380d1cd025700c899aa70e
MichaelOsorio2017 <ms.osorio@uniandes.edu.co>
2019-03-28 21:38:02 -0500

    buscar medio implementado

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import './css/Style.css';
 import './css/_timerSty.css';
 import App from './App.js'
+import HostalData from './MainView/HostalData';
 
 ReactDOM.render(<App />, document.getElementById('root'));
