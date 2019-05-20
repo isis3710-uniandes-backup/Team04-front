850ae74a5f562dfe6075610bdf4af4cd88d1695c
Alejandro Garcia Flores <a.garcia13@uniandes.edu.co>
2019-05-20 03:48:51 -0500

    update service worker dev

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import {IntlProvider, addLocaleData} from 'react-intl';
 import esLocaleData from 'react-intl/locale-data/es';
 import localeEsMessages from "./locales/es";
 import localeEnMessages from "./locales/en";
+import * as serviceWorker from './serviceWorker';
 
 
 addLocaleData(esLocaleData);
@@ -22,3 +23,7 @@ ReactDOM.render(
     <IntlProvider locale={userLang} messages= {getLocale()}>
         <App/>
     </IntlProvider>, document.getElementById("root"));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
